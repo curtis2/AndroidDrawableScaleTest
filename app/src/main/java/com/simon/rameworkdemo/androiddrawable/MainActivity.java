14701a4ea509c9a700f9685a0aa2e7d73db6af5f
curtis2 <18292967668@163.com>
2017-09-27 16:59:08 +0800

    修改项目测试代码

--- app/src/main/java/com/simon/rameworkdemo/androiddrawable/MainActivity.java
+++ app/src/main/java/com/simon/rameworkdemo/androiddrawable/MainActivity.java
@@ -18,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void buttonClick(View view) {
-        //图片本身大小为 532x400 放置在xhdpi下，运行在xhdpi（240dpi ~ 320dpi）密度的设备上，显示的大小为532x400
-        //图片本身大小为 532x400 放置在xhdpi下，运行在hdpi（160dpi ~ 240dpi）密度的设备上，图片被放大，显示的大小为709x533
-        //图片本身大小为 532x400 放置在xhdpi下，运行在xxhdpi(320dpi ~ 480dpi)密度的设备上，图片被缩小，显示的大小为355x267
         Toast.makeText(this, "图片宽度：" + imageView.getWidth(), Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "图片高度：" + imageView.getHeight(), Toast.LENGTH_SHORT).show();
     }
